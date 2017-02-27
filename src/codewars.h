75ae7ad2d0747f4cf91cc5e4f7a94f7304e81ba5
Padraic Cashin <cashinpj@clarkson.edu>
2017-02-27 10:22:31 -0700

    Added constants to codwars.h

--- src/codewars.h
+++ src/codewars.h
@@ -1,36 +1,40 @@
 #ifndef _CODEWARS_H_
 #define _CODEWARS_H_
+	
+const int MAX_CODEWARS_INSTRUCTION = 20;
+const int MAX_ARGUMENT_MOD = 7;
+const int MAX_MEMORY = 8000;
 
-	enum Instructions {
-		DAT = 0,
-		MOV,
-		ADD,
-		SUB,
-		MUL,
-		DIV,
-		MOD,
-		JMP,
-		JMZ,
-		JMN,
-		DJN,
-		SPL,
-		CMP,
-		SEQ,
-		SNE,
-		SLT,
-		LDP,
-		STP,
-		NOP
-	};
+enum Instructions {
+	DAT = 0,
+	MOV,
+	ADD,
+	SUB,
+	MUL,
+	DIV,
+	MOD,
+	JMP,
+	JMZ,
+	JMN,
+	DJN,
+	SPL,
+	CMP,
+	SEQ,
+	SNE,
+	SLT,
+	LDP,
+	STP,
+	NOP
+};
 
-	typedef enum AddressModes {
-		IMMEDIATE = 0,
-		DIRECT,
-		INDIRECT,
-		INDIR_PREDEC,
-		INDIR_A,
-		INDIR_PREDEC_A,
-		INDIR_POSDEC_A
-	} addmode;
+typedef enum AddressModes {
+	IMMEDIATE = 0,
+	DIRECT,
+	INDIRECT,
+	INDIR_PREDEC,
+	INDIR_A,
+	INDIR_PREDEC_A,
+	INDIR_POSDEC_A
+} addmode;
 
 #endif
