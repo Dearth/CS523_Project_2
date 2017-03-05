75362f7fa8cebf4c6008e61105dcf6883473093a
Padraic Cashin <cashinpj@clarkson.edu>
2017-03-04 19:23:17 -0700

    Moving genome from pointers to pass by ref to prevent segfaults

--- src/genome.h
+++ src/genome.h
@@ -13,12 +13,7 @@ typedef struct Gene {
 
 typedef std::vector<Gene> genome;
 
-//create a genome and seed random generator
-genome* initGenome() {
-	return new genome;
-}
-
-void genoToPheno(genome* _genome,string name){
+void genoToPheno(genome& _genome,string name){
 
 	ofstream outfile;
 	outfile.open(name.c_str(), std::ofstream::trunc);
@@ -31,7 +26,7 @@ void genoToPheno(genome* _genome,string name){
 	outfile << ";name " << name	<< std::endl;
 	outfile << ";author " << author	<< std::endl;
 	outfile << ";assert CORESIZE==8000" << std::endl;
-	for(Gene gene: *_genome) {
+	for(Gene gene: _genome) {
 		outfile << instructions[gene.instruction_] << " "
 			<< addressMode[gene.arg_mod_one_] << gene.arg_one_<<", "
 			<< addressMode[gene.arg_mod_two_] << gene.arg_two_<<endl;
@@ -57,25 +52,25 @@ gene_s createNewGene() {
 }
 
 //randomly change the instruction
-void mutate_instruction(gene_s* g) {
-  g->instruction_ = static_cast<Instructions>(rand()%MAX_CODEWARS_INSTRUCTION);
+void mutate_instruction(gene_s& g) {
+  g.instruction_ = static_cast<Instructions>(rand()%MAX_CODEWARS_INSTRUCTION);
 }
 
 //randomly change one of the arguments
-void mutate_argument(gene_s* g) {
+void mutate_argument(gene_s& g) {
 	if(rand()%2 == 0) {
-		g->arg_one_ = rand()%MAX_MEMORY;
+		g.arg_one_ = rand()%MAX_MEMORY;
 	} else {
-		g->arg_two_ = rand()%MAX_MEMORY;
+		g.arg_two_ = rand()%MAX_MEMORY;
 	}
 }
 
 //randomly change one of the argument modes
-void mutate_arg_mode(gene_s* g) {
+void mutate_arg_mode(gene_s& g) {
 	if(rand()%2 == 0) {
-		g->arg_mod_one_ = static_cast<addmode>(rand()%MAX_ARGUMENT_MOD);
+		g.arg_mod_one_ = static_cast<addmode>(rand()%MAX_ARGUMENT_MOD);
 	} else {
-		g->arg_mod_two_ = static_cast<addmode>(rand()%MAX_ARGUMENT_MOD);
+		g.arg_mod_two_ = static_cast<addmode>(rand()%MAX_ARGUMENT_MOD);
 	}
 }
 
@@ -105,7 +100,7 @@ void mutate_arg_mode(gene_s* g) {
 		.
 */
 
-void mutateGenome(genome* g, 	const int add_gene, 
+void mutateGenome(genome& g, 	const int add_gene, 
 								const int swap_gene, 
 								const int del_gene, 
 								const int ins_gene, 
@@ -118,9 +113,9 @@ void mutateGenome(genome* g, 	const int add_gene,
 	int mutation = (rand()%100) + 1;
 	int index_one, index_two;
 
-	if(g->size() != 0) {
-		index_one = rand()%g->size();
-		index_two = rand()%g->size();
+	if(g.size() != 0) {
+		index_one = rand()%g.size();
+		index_two = rand()%g.size();
 	}
 
 	//generate cummulative percent chances for gene mutation
@@ -131,39 +126,39 @@ void mutateGenome(genome* g, 	const int add_gene,
 
 	if(mutation <= add_gene) {
 		//append a new gene
-		g->push_back(createNewGene());
+		g.push_back(createNewGene());
 
 	} else if (mutation <= swap_percent) {
 		//swap genes at two locations
-		if(g->size() < 2) {
+		if(g.size() < 2) {
 			return;
 		}
 
-		auto first = g->begin() + index_one;
-		auto second = g->begin() + index_two;
+		auto first = g.begin() + index_one;
+		auto second = g.begin() + index_two;
 
 		iter_swap(first, second);
 	
 	} else if (mutation <= del_percent) {
 		//delete gene
-		if(g->size() == 0) {
+		if(g.size() == 0) {
 			return;
 		}
 
-		auto loc = g->begin() + index_one;
+		auto loc = g.begin() + index_one;
 
-		g->erase(loc);
+		g.erase(loc);
 
 	} else if (mutation <= ins_percent) {
 		//insert gene
-		auto loc = g->begin() + index_one;
+		auto loc = g.begin() + index_one;
 
-		g->insert(loc, createNewGene());
+		g.insert(loc, createNewGene());
 
 	} else if (mutation <= change_percent) {
 		//change gene
 		//calculate cummulative percentages for mutate
-		if(g->size() == 0) {
+		if(g.size() == 0) {
 			return;
 		}
 
@@ -177,15 +172,15 @@ void mutateGenome(genome* g, 	const int add_gene,
 			//mutute the gene pointed to by the begin iterator offset by index
 			//dereference the iterator to get the memory location of the gene
 			//instead of memory location of iterator
-			mutate_instruction(&(*(g->begin()+index_one)));
+			mutate_instruction(*(g.begin()+index_one));
 
 		} else if (mutation <= mode_percent) {
 			//change mode
-			mutate_arg_mode(&(*(g->begin()+index_one)));
+			mutate_arg_mode(*(g.begin()+index_one));
 
 		} else if (mutation <= addr_percent) {
 			//change address
-			mutate_argument(&(*(g->begin()+index_one)));
+			mutate_argument(*(g.begin()+index_one));
 
 		} else {
 			cerr << "Gene mutation percentages add up to less than 100%" << endl; 
@@ -196,17 +191,17 @@ void mutateGenome(genome* g, 	const int add_gene,
 	}
 }
 
-genome* onePointCrossover(genome* g1, genome* g2) {
-	int s1 = g1->size();
-	int s2 = g2->size();
+genome onePointCrossover(genome& g1, genome& g2) {
+	int s1 = g1.size();
+	int s2 = g2.size();
 	int pivot = 0;
 
-	genome* g = new genome;
+	genome g;
 
 	if(s1 == 0) {
-		*g = *g2;
+		g = g2;
 	} else if (s2 == 0) {
-		*g = *g1;
+		g = g1;
 	} else {
 
 		//use smaller vector for pivot point
@@ -218,61 +213,61 @@ genome* onePointCrossover(genome* g1, genome* g2) {
 
 		if(rand()%2 == 0) {
 			for(int i = 0; i < pivot; ++i) {
-				g->push_back(g1->at(i));
+				g.push_back(g1.at(i));
 			}
 	
 			for(int i = pivot + 1; i < s2; ++i) {
-				g->push_back(g2->at(i));
+				g.push_back(g2.at(i));
 			}
 		} else {
 			for(int i = 0; i < pivot; ++i) {
-				g->push_back(g2->at(i));
+				g.push_back(g2.at(i));
 			}
 
 			for(int i = pivot + 1; i < s1; ++i) {
-				g->push_back(g1->at(i));
+				g.push_back(g1.at(i));
 			}
 		}
 	}
 	return g;
 }
 
-genome* uniformCrossover(genome* g1, genome* g2) {
-	int s1 = g1->size();
-	int s2 = g2->size();
+genome uniformCrossover(genome& g1, genome& g2) {
+	int s1 = g1.size();
+	int s2 = g2.size();
 	int pivot = 0;
 
-	genome* g = new genome;
+	genome g;
 
 	if(s1 == 0) {
-		*g = *g2;
+		g = g2;
 	} else if (s2 == 0) {
-		*g = *g1;
+		g = g1;
 	} else {
 		if (s1 < s2) {
 			for(int i = 0; i < s1; ++i) {
 				if(rand()%2) {
-					g->push_back(g1->at(i));
+					g.push_back(g1.at(i));
 				} else {
-					g->push_back(g2->at(i));
+					g.push_back(g2.at(i));
 				}
 			}
 	
 			for(int i = s1; i < s2; ++i) {
-				g->push_back(g2->at(i));
+				g.push_back(g2.at(i));
 			}
 
 		} else {
 			for(int i = 0; i < s2; ++i) {
 				if(rand()%2) {
-					g->push_back(g1->at(i));
+					g.push_back(g1.at(i));
 				} else {
-					g->push_back(g2->at(i));
+					g.push_back(g2.at(i));
 				}
 			}
 	
 			for(int i = s2; i < s1; ++i) {
-				g->push_back(g1->at(i));
+				g.push_back(g1.at(i));
 			}
 		}
 	}
