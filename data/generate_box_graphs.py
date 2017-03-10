5c95b9c7b599de9755c4043c632ca836b56feca9
Padraic Cashin <cashinpj@clarkson.edu>
2017-03-10 15:27:14 -0700

    Added generate_box_graphs

--- data/generate_box_graphs.py
+++ data/generate_box_graphs.py
@@ -76,26 +76,64 @@ def plot_convergence(data, l1, l2, l3, title):
 	plt.subplot(211)
 	plt.title(title)
 	plt.plot(g, data[0], label=l1)
-	plt.plot(g, data[1], label=l2)
-	plt.plot(g, data[2], label=l3)
+	plt.plot(g, data[1], '-.', label=l2)
+	plt.plot(g, data[2], '--', label=l3)
 	plt.legend()
 	plt.xlabel('Generation', fontsize=14)
 	plt.ylabel('Average Fitness', fontsize=14)
 	
 	plt.subplot(212)
 	plt.plot(g, diffs[0], label=l1) 
-	plt.plot(g, diffs[1], label=l2)
-	plt.plot(g, diffs[2], label=l3)
+	plt.plot(g, diffs[1], '-.', label=l2)
+	plt.plot(g, diffs[2], '--', label=l3)
 	plt.ylabel('Distance between Generations', fontsize=14)
 
 	plt.show()
 
 	return
 
+def plot_convergence(data, l1, l2, l3, l4, l5, title):
+    g=range(1,101)
+
+    diffs = []
+    for block in data:
+        trial_diff = []
+        previous = 0
+
+        for value in block:
+            trial_diff.append(math.sqrt((previous ** 2) + (value ** 2)))
+            previous = value
+
+        diffs.append(trial_diff)
+
+    plt.subplot(211)
+    plt.title(title)
+    plt.plot(g, data[0], label=l1)
+    plt.plot(g, data[1], '-.', label=l2)
+    plt.plot(g, data[2], '--', label=l3)
+    plt.plot(g, data[3], '.', label=l4)
+    plt.plot(g, data[4], '.-', label=l5)
+    plt.legend()
+    plt.xlabel('Generation', fontsize=14)
+    plt.ylabel('Average Fitness', fontsize=14)
+
+    plt.subplot(212)
+    plt.plot(g, diffs[0], label=l1)
+    plt.plot(g, diffs[1], '-.', label=l2)
+    plt.plot(g, diffs[2], '--', label=l3)
+    plt.plot(g, diffs[3], '.', label=l4)
+    plt.plot(g, diffs[4], '.-', label=l5)
+    plt.ylabel('Distance between Generations', fontsize=14)
+
+    plt.show()
+
+    return
+
 def plot_box(data,l1,l2,l3,title):
 	plt.figure()
 	plt.title(title)
 	plt.boxplot(data, labels={l1, l2, l3}, notch=False, sym='+', vert=True, whis=1.5, positions=None, widths=None, patch_artist=False, bootstrap=None, usermedians=None, conf_intervals=None)
+	plt.ylabel('Fitness')
 	plt.show()
 	
 	p1 = ranksums(data[0], data[1])
@@ -108,20 +146,49 @@ def plot_box(data,l1,l2,l3,title):
 
 	return
 
+def plot_box(data,l1,l2,l3,l4,l5,title):
+    plt.figure()
+    plt.title(title)
+    plt.boxplot(data, labels={l1, l2, l3, l4, l5}, notch=False, sym='+', vert=True, whis=1.5, positions=None, widths=None, patch_artist=False, bootstrap=None, usermedians=None, conf_intervals=None)
+    plt.ylabel('Fitness')
+    plt.show()
+
+    return
+
 def find_fit(data):
-	return []
+	fit = []
+
+	for block in data:
+		block_fit = []
+		for trial in block:
+			for gen in range(90, len(trial)):
+				block_fit.append(trial[gen][-1]);
+
+		fit.append(block_fit)
+
+	return fit
 
 def main():
 	data = []
+	max_range = 4	
+
+	if len(sys.argv) == 13:
+		max_range = 6
 	
-	for i in range(1, 4):
+	for i in range(1, max_range):
 		data.append(clean_data(sys.argv[i]))
 
 	avg_data = find_avgs(data)
 	most_fit = find_fit(data)
 
-	plot_convergence(avg_data, sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
-	plot_box(most_fit, sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[8])
+	if len(data) == 3:
+		plot_convergence(avg_data, sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+		plot_box(most_fit, sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[8])
+
+	else:
+		plot_convergence(avg_data, sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[11])
+		plot_box(most_fit, sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[12])
+		
 
 if __name__ == "__main__":
 	main()	
